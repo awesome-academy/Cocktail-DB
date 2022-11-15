6c540643289a62647493a790a25f94170cb63586
hunggld <le.duong.hung@sun-asterisk.com>
2022-11-15 13:52:47 +0700

    init project

---
+++ app/src/main/java/com/sildev/thecocktail/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sildev.thecocktail.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sildev.thecocktail.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
